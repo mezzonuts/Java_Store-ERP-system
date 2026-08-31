7588e1b9ac5813a0f0b6b58f8839bc671cec6de0
Andika gumilar <65252469+mezzonuts@users.noreply.github.com>
2026-08-31 22:37:57 +0700

    fix: set full PATH to Maven in run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,7 @@
 @echo off
 cd /d D:\try_my_project\ERP_JAVA
 set JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-21.0.12.101-hotspot
-set PATH=%JAVA_HOME%\bin;%PATH%
-mvn -DskipTests clean compile exec:java "-Dexec.mainClass=com.sosha.SoshaApp" "-Dspring.profiles.active=sqlite"
+set PATH=%JAVA_HOME%\bin;C:\maven\apache-maven-3.9.6\bin;%PATH%
+echo Running Sosha POS...
+call mvn -DskipTests clean compile exec:java "-Dexec.mainClass=com.sosha.SoshaApp" "-Dspring.profiles.active=sqlite"
 pause

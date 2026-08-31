48d0cd716d6190379b0c441893a90db47d6782fa
Andika gumilar <65252469+mezzonuts@users.noreply.github.com>
2026-08-31 22:52:23 +0700

    fix: add cmd execution hint in run.bat

--- run.bat
+++ run.bat
@@ -2,6 +2,7 @@
 cd /d D:\try_my_project\ERP_JAVA
 set JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-21.0.12.101-hotspot
 set PATH=%JAVA_HOME%\bin;C:\maven\apache-maven-3.9.6\bin;%PATH%
-echo Running Sosha POS...
+echo Running Sosha POS with cmd...
+echo Note: If using PowerShell, run: cmd /c "D:\try_my_project\ERP_JAVA\run.bat"
 call mvn -DskipTests clean compile exec:java "-Dexec.mainClass=com.sosha.SoshaApp" "-Dspring.profiles.active=sqlite"
 pause
